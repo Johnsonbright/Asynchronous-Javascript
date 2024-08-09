38535fad943d9e84700c99506e3f8b9b7ca4dc1e
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-08-09 14:24:38 +0100

    a glimpse into callback hell

--- starter/script.js
+++ starter/script.js
@@ -3,38 +3,75 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 ///////////////////////////////////////////////////
 
-const getCountry = function(country) {
-const request = new XMLHttpRequest();
-request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-request.send();
-
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-
+const renderCountry = function (data, className = "") {
   const html = `
-    <article class="country">
-      <img src="${data.flags.png}" alt="" class="country_img" />
-      <div class="country_data">
-        <h3 class="country_name">${data.name.official}</h3>
-        <h4 class="country_region"> ${data.region} </h4>
-        <p class="country_row">
-          <span> ${(+data.population/1000000).toFixed(2)} People 👨‍👨‍👧 </span>
-        </p>
-        <p class="country_row">
-          <span> ${data.languages}🧏🏻‍♂️</span>
-        </p>
-        <p class="country_row">
-          <span> ${data.currencies?.EUR.name} 💰</span>
-        </p>
-      </div>
-    </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
+  <article class="country ${className}">
+    <img src="${data.flags.png}" alt="" class="country_img" />
+    <div class="country_data">
+      <h3 class="country_name">${data.name.official}</h3>
+      <h4 class="country_region"> ${data.region} </h4>
+      <p class="country_row">
+        <span>👨‍👨‍👧</span>
+        ${(+data.population / 1000000).toFixed(2)} People
+      </p>
+      <p class="country_row">
+        <span>🧏🏻‍♂️</span>
+        ${data.languages}
+      </p>
+      <p class="country_row">
+        <span>💰</span>
+        ${data.currencies?.EUR.name}
+      </p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
   countriesContainer.style.opacity = 1;
-});
+};
+const getCountryAndNeighbouring = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //Render country
+    renderCountry(data);
+
+    //Get neighbour country 2
+    const neighbour = data.borders;
+    if (!neighbour) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, "neighbour");
+    });
+  });
 };
 
-getCountry('portugal')
-getCountry('us')
+getCountryAndNeighbouring("portugal");
 
+setTimeout(() => {
+  console.log("1 seconds passed");
 
+  setTimeout(() => {
+    console.log("2 seconds passed");
+
+    setTimeout(() => {
+      console.log("3 seconds passed");
+
+      
+setTimeout(()=> {
+  console.log('4 seconds passed')
+}, 1000)
+    }, 1000);
+  }, 1000);
+}, 1000);
