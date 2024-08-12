d0d47ee40b03ee93bb35b755990dcd329543ddaf
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-08-12 15:39:27 +0100

    rendering my geo location implementing the new Promise(resolve, reject)

--- starter/script.js
+++ starter/script.js
@@ -1,45 +1,141 @@
 "use strict";
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
-//BUILDING SIMPLE PROMISES
-const lotteryDraw = new Promise(function(resolve, reject) {
-  console.log(`Lottery draw is happening`)
-  setTimeout(()=> {
-     
-    if ( Math.random() >= 0.5 ) {
-      resolve( `You win`)
-    }
-    else{
-      reject(new Error(`You lose`))
-    }
-  }, 2000)
 
-});
+// RENDERING MY OWN GEO-LOCATION
 
-lotteryDraw.then(res => console.log(res)).catch(err => console.error(err));
-
-
-const wait = function(seconds) {
-  return new Promise (function(resolve) {
-    setTimeout(resolve, seconds * 1000);
+//
+const getPosition = function () {
+  return new Promise((resolve, reject) => {
+    // navigator.geolocation.getCurrentPosition(position => resolve(position), err => reject(err));
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
-wait(2).then(()=> {
-  console.log('I waited for 1 seconds');
-return wait(1)
-}).then(()=> {
-  console.log('I waited for 2 seconds');
-return wait(1)
-}).then(()=> {
-  console.log('I waited for 3 seconds');
-return wait(1)
-}).then(() => {
-  console.log(`I waited for 4 second`)
-})
+// getPosition().then(pos => console.log(pos))
 
-// Another to resolve immediately
-Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error(`Problem`)).catch(x => console.error(x))
+
+const renderCountry = function (data, className = "") {
+  // console.log(data);
+  const html = `
+      <article class="country ${className}">
+        <img src="${
+          Object.values(data.flags ?? {})[0]
+        }" alt="" class="country_img" />
+        <div class="country_data">
+          <h3 class="country_name">${Object.values(data.name ?? {})[0]}</h3>
+          <h4 class="country_region"> ${data.region} </h4>
+          <p class="country_row">
+            <span>👨‍👨‍👧</span>
+            ${(+data.population / 1000000).toFixed(2)} People
+          </p>
+          <p class="country_row">
+            <span>🧏🏻‍♂️</span>
+            ${Object.values(data.languages ?? {})[0]} 
+          </p>
+          <p class="country_row">
+            <span>💰</span>
+            ${Object.keys(data.currencies ?? {})[0]}
+          </p>
+        </div>
+      </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+   countriesContainer.style.opacity = 1;
+};
+const renderError = function (message) {
+  countriesContainer.insertAdjacentText("beforeend", message);
+  //  countriesContainer.style.opacity = 1;
+};
+
+// HERE:
+const whereAmI = function () {
+  getPosition()
+    .then((pos) => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}&api_key=66b8c191808f7674335948vpfa1908f`
+      );
+    })
+    .then((response) => {
+      // console.log(response)
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      console.log(`You are in ${data.address.city ?? data.address.state}, ${data.address.country}`)
+      return getCountryData(data.address.country);
+    })
+    .catch((err) => console.error(` Something went wrong ${err}`));
+};
+
+
+const getJSON = function (url, errorMessage = "Something went wrong.") {
+  return fetch(url).then((response) => {
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`${errorMessage} ${response.status}`);
+    }
+
+    return response.json();
+  });
+};
+
+const getCountryData = function (country) {
+  // country 1
+  console.log(country);
+  return getJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    `Can not access country`
+  ).then((data) => {
+   return renderCountry(data[0]);
+  }).catch(err => console.error(`${err.message}`))
+};
+
+
+btn.addEventListener("click", whereAmI);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//BUILDING SIMPLE PROMISES
+// const lotteryDraw = new Promise(function(resolve, reject) {
+//   console.log(`Lottery draw is happening`)
+//   setTimeout(()=> {
+
+//     if ( Math.random() >= 0.5 ) {
+//       resolve( `You win`)
+//     }
+//     else{
+//       reject(new Error(`You lose`))
+//     }
+//   }, 2000)
+
+// });
+
+// lotteryDraw.then(res => console.log(res)).catch(err => console.error(err));
+
+// const wait = function(seconds) {
+//   return new Promise (function(resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+// wait(2).then(()=> {
+//   console.log('I waited for 1 seconds');
+// return wait(1)
+// }).then(()=> {
+//   console.log('I waited for 2 seconds');
+// return wait(1)
+// }).then(()=> {
+//   console.log('I waited for 3 seconds');
+// return wait(1)
+// }).then(() => {
+//   console.log(`I waited for 4 second`)
+// })
+
+// // Another to resolve immediately
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error(`Problem`)).catch(x => console.error(x))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // QUIZ
 
 // const renderCountry = function (data, className = "") {
@@ -56,7 +152,7 @@ Promise.reject(new Error(`Problem`)).catch(x => console.error(x))
 //       </p>
 //       <p class="country_row">
 //         <span>🧏🏻‍♂️</span>
-//         ${Object.values(data.languages ?? {})[0]} 
+//         ${Object.values(data.languages ?? {})[0]}
 //       </p>
 //       <p class="country_row">
 //         <span>💰</span>
@@ -93,7 +189,7 @@ Promise.reject(new Error(`Problem`)).catch(x => console.error(x))
 //     .then((data) => {
 //       console.log(data[0]);
 //       renderCountry(data?.[0]);
-      
+
 //       const neighbor = data?.[0].borders[1];
 
 //       if (!neighbor) throw new Error(`No neighbor found`);
@@ -141,7 +237,6 @@ Promise.reject(new Error(`Problem`)).catch(x => console.error(x))
 //   // whereAmI(6.5244, 3.406448);
 //   // whereAmI(39.3999, 8.2245);
 // });
-
 
 ///////////////////////////////////////////////////
 // const renderCountry = function (data, className = "") {
